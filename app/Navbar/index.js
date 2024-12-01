9c19b6363871d9078c420cf055f68f8d45c34d17
Isaac Longoria <isaac.longoria9136@gmail.com>
2024-12-01 16:59:35 -0600

    mobile menu

---
+++ app/Navbar/index.js
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import {Button, ButtonGroup} from "@nextui-org/button";
+import Link from 'next/link';
+import lottie from 'lottie-web';
+import menuAnimationData from '../../public/Menu.json';
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    playerRef.current = lottie.loadAnimation({
+      container: document.querySelector("#menu-icon"),
+      animationData: menuAnimationData,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+    });
+  }, []);
+
+  const toggleMenu = () => {
+    if (menuOpen) {
+      // Play the second half of the animation
+      playerRef.current.playSegments([30, 60], true);
+    } else {
+      // Play the first half of the animation and stop at frame 30
+      playerRef.current.playSegments([0, 30], true);
+      setTimeout(() => {
+        playerRef.current.goToAndStop(27, true);
+      }, 490); // Adjust the timing to match the duration of the first half of the animation
+    }
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className="relative">
+      <div className='w-full bg-[#292732] z-30'>
+        <div className='absolute z-20 w-full bg-[#292732] border-x-[1px] border-gray-500'>
+          <div className='flex flex-row justify-between h-45 px-6 py-[13px] pt-[30px]'>
+            <Image
+              src="/SiteLogo-mobile.svg"
+              alt="Logo"
+              width={100}
+              height={100}
+            />
+            <div onClick={toggleMenu} className="cursor-pointer" id="menu-icon" style={{ height: '50px', width: '50px' }}>
+              {/* The Lottie animation will be rendered here */}
+            </div>
+          </div>
+        </div>
+        <div className={`absolute top-[90px] w-full h-[275px] flex flex-col justify-between px-6 py-6 bg-[#292732] border-[1px] border-t-0 border-gray-500 rounded-b-[13px] drop-shadow-[0_14px_16.2px_rgba(0,0,0,0.25)] backdrop-blur-[7px] transition-transform duration-300 z-10 ${menuOpen ? 'translate-y-0' : '-translate-y-[260px]'}`}>
+          <Link href="#" className="font-maven-pro text-white text-[14px] font-bold block mb-2">Our Why</Link>
+          <Link href="#" className="font-maven-pro text-white text-[14px] font-bold block mb-2">Services</Link>
+          <Link href="#" className="font-maven-pro text-white text-[14px] font-bold block mb-2">Pricing</Link>
+          <Link href="#" className="font-maven-pro text-white text-[14px] font-bold block mb-2">Team</Link>
+          <Button
+            href="#"
+            className="w-full h-[45px] font-maven-pro text-white text-[14px] font-bold rounded-[20px] bg-green-500 mt-4">
+            Book a Call
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,65 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'home-pattern': "url('/Backgrounds.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        green: {
+          200: '#D5FEEE',
+          300: '#65EDB7',
+          500: '#00CF7D',
+          700: '#179865',
+        },
+        blue: {
+          300: '#46C1E3',
+          500: '#0096C0',
+          700: '#05708E',
+        },
+        purple: {
+          300: '#D0DBFB',
+          500: '#7985CC',
+          700: '#4054AD',
+          800: '#304089',
+        },
+        baby_blue: {
+          300: '#C3EDFF',
+          500: '#80D8FE',
+          700: '#40BCF2',
+        },
+        yellow: {
+          300: '#FFF4C7',
+          500: '#FFE993',
+          700: '#CCBD82',
+        },
+        gray: {
+          100: '#',
+          200: '#E3EBED',
+          300: '#A4A1AE',
+          400: '#928F9F',
+          500: '#605E67',
+          600: '#',
+          700: '#4B4952',
+          800: '#37363F',
+          900: '#292732',
+        },
+      },
+      fontFamily: {
+        'maven-pro': ['"Maven Pro"', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
       },
     },
   },
