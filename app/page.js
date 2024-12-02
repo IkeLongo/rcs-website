99b25f07e09fe3b2f1e1c2e7834676e276c68792
Isaac Longoria <isaac.longoria9136@gmail.com>
2024-12-01 18:30:48 -0600

    Added gif to hero section

--- app/page.js
+++ app/page.js
@@ -14,8 +14,8 @@ export default function Home() {
           height={653}
           className="absolute left-10 top-0"
         />
-        <div className="absolute top-36 left-16 z-40 p-6">
-          <h1 className="w-[292px] text-[32px] text-gray-900 font-gentium-book-plus font-bold text-right">Crafting <span className='italic text-blue-300'>Powerful</span> Websites and Branding for Your Business</h1>
+        <div className="absolute top-32 left-16 z-40 p-6">
+          <h1 className="w-[292px] text-[28px] text-gray-900 font-gentium-book-plus font-bold text-right">Crafting <span className='italic text-blue-300'>Powerful</span><br />Websites and<br />Branding for<br />Your Business</h1>
           <p className="text-gray-800 text-[14px] text-right mt-4">Propel your business forward with<br />innovative web solutions, reliable<br />hosting, and impactful branding.</p>
           <div className="flex justify-end mt-4">
             <Button
@@ -24,6 +24,19 @@ export default function Home() {
               Book a Call
             </Button>
           </div>
+        </div>
+        <div className="absolute left-[10%] top-[100%]">
+          <Image
+            src="/home-hero-gif-blob.svg"
+            alt="Hero gif background blob"
+            width={224}
+            height={212}
+          />
+          <img
+            src="/home-page-gif.gif"
+            alt="Home page gif"
+            className="absolute top-0 left-0 w-full h-full"
+          />
         </div>
       </div>
       {/* <h1 className="text-4xl text-center text-white pt-10">Welcome to Next.js</h1> */}
