676089d53766c177b7e249074815517653e591d0
Isaac Longoria <isaac.longoria9136@gmail.com>
2024-12-01 18:37:42 -0600

    Adjusted position of hero section gif.

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
           height={653}
           className="absolute left-10 top-0"
         />
-        <div className="absolute top-32 left-16 z-40 p-6">
+        <div className="absolute top-32 left-16 z-0 p-6">
           <h1 className="w-[292px] text-[28px] text-gray-900 font-gentium-book-plus font-bold text-right">Crafting <span className='italic text-blue-300'>Powerful</span><br />Websites and<br />Branding for<br />Your Business</h1>
           <p className="text-gray-800 text-[14px] text-right mt-4">Propel your business forward with<br />innovative web solutions, reliable<br />hosting, and impactful branding.</p>
           <div className="flex justify-end mt-4">
@@ -25,7 +25,7 @@ export default function Home() {
             </Button>
           </div>
         </div>
-        <div className="absolute left-[10%] top-[80%]">
+        <div className="absolute left-[10%] top-[65%]">
           <Image
             src="/home-hero-gif-blob.svg"
             alt="Hero gif background blob"
