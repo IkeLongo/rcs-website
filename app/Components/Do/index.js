2d72352587f196135c60eef0ad61a4ea29341f8a
Isaac Longoria <isaac.longoria9136@gmail.com>
2024-12-02 11:04:26 -0600

    What we do third commit.

--- app/Components/Do/index.js
+++ app/Components/Do/index.js
@@ -2,33 +2,31 @@ import Image from 'next/image';
 
 export default function Do({ icon, title, description }) {
 	return (
-		<div>
-			<div className="w-[233px] p-6 flex flex-col justify-center gap-4 items-stretch rounded-[14px] border border-gray-500 bg-do-custom-gradient">
-				<div className='flex flex-col items-center'>
-					<Image 
-						src={icon}
-						alt="Icon"
-						width={60.25}
-						height={60.25}
-						className=""
-					/>
-					<h3 className="font-gentium-book-plus text-white text-[16px] font-bold text-center">{title}</h3>
-				</div>
-				<p className="font-harmattan text-center text-[16px]">
-        	{description}
-      	</p>
-				<div className='flex justify-center gap-1'>
-					<p className='font-harmattan uppercase text-[14px]'>
-						Learn More
-					</p>
-					<Image 
-						src="/arrow-circle-right.svg"
-						alt="Right arrow"
-						width={16.87}
-						height={16.87}
-						className=""
-					/>
-				</div>
+		<div className="min-w-[233px] p-6 flex flex-col justify-between gap-4 items-stretch rounded-[14px] border border-gray-500 bg-do-custom-gradient">
+			<div className='flex flex-col items-center'>
+				<Image 
+					src={icon}
+					alt="Icon"
+					width={60.25}
+					height={60.25}
+					className=""
+				/>
+				<h3 className="font-gentium-book-plus text-white text-[16px] font-bold text-center">{title}</h3>
+			</div>
+			<p className="font-harmattan text-center text-[16px] flex-grow">
+				{description}
+			</p>
+			<div className='flex justify-center gap-1'>
+				<p className='font-harmattan uppercase text-[14px]'>
+					Learn More
+				</p>
+				<Image 
+					src="/arrow-circle-right.svg"
+					alt="Right arrow"
+					width={16.87}
+					height={16.87}
+					className=""
+				/>
 			</div>
 		</div>
 	);
--- app/page.js
+++ app/page.js
@@ -89,21 +89,21 @@ export default function Home() {
             What We Do
           </h2>
           <div className="overflow-x-auto w-full px-4">
-            <div className="flex gap-[19px] min-h-[510px]">
+            <div className="flex gap-[19px] min-h-[470px]">
               <Do 
                 icon="/home-do-web.gif"
                 title="Website Design & Development"
                 description="Our Web Design & Development package delivers captivating designs tailored to engage and convert your audience. Each website is crafted with strategic attention to detail, bringing your digital vision to life with a high-performing, user-friendly website on all devices."
               />
               <Do 
-                icon="/home-do-web.gif"
-                title="Website Design & Development"
-                description="Our Web Design & Development package delivers captivating designs tailored to engage and convert your audience. Each website is crafted with strategic attention to detail, bringing your digital vision to life with a high-performing, user-friendly website on all devices."
+                icon="/home-do-brand.gif"
+                title="Branding & Visual Identity Systems"
+                description="Our Branding & Visual Identity Systems package creates a cohesive identity that resonates with your audience. From custom logos to color palettes and iconography, we build a distinctive visual story to elevate your brand and leave a lasting impression across all platforms."
               />
               <Do 
-                icon="/home-do-web.gif"
-                title="Website Design & Development"
-                description="Our Web Design & Development package delivers captivating designs tailored to engage and convert your audience. Each website is crafted with strategic attention to detail, bringing your digital vision to life with a high-performing, user-friendly website on all devices."
+                icon="/home-do-host.gif"
+                title="Hosting, Maintenance & Security "
+                description="Simplify website management with our Hosting, Maintenance, and Security package, which covers updates, security, and protection. Our package ensures that your site runs smoothly and securely, giving you peace of mind letting you focus on your business."
               />
             </div>
           </div>
