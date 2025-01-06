6b02e4fbfbf6aee3e9285e8e2d5f7fa0e2d337e6
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-06 10:15:37 -0600

    What we do scroll adjustment v6

--- app/Components/DoContainer/index.js
+++ app/Components/DoContainer/index.js
@@ -22,35 +22,33 @@ export default function DoContainer() {
 
   return (
     <div className="w-full overflow-x-auto overflow-y-hidden">
-      <div className="flex">
-        <div className="flex gap-[19px] min-h-[470px]">
-          <div onClick={() => scrollToId('block-1')} className="flex min-w-full">
-            <Do 
-              icon="/home-do-web.gif"
-              title="Website Design & Development"
-              description="Our Web Design & Development package delivers captivating designs tailored to engage and convert your audience. Each website is crafted with strategic attention to detail, bringing your digital vision to life with a high-performing, user-friendly website on all devices."
-              className="min-w-[300px] flex-shrink-0"
-              link="/services#block-1"
-            />
-          </div>
-          <div onClick={() => scrollToId('block-2')} className="flex min-w-full">
-            <Do 
-              icon="/home-do-brand.gif"
-              title="Branding & Visual Identity Systems"
-              description="Our Branding & Visual Identity Systems package creates a cohesive identity that resonates with your audience. From custom logos to color palettes and iconography, we build a distinctive visual story to elevate your brand and leave a lasting impression across all platforms."
-              className="min-w-[300px] flex-shrink-0"
-              link="/services#block-2"
-            />
-          </div>
-          <div onClick={() => scrollToId('block-3')} className="flex min-w-full">
-            <Do 
-              icon="/home-do-host.gif"
-              title="Hosting, Maintenance & Security"
-              description="Simplify website management with our Hosting, Maintenance, and Security package, which covers updates, security, and protection. Our package ensures that your site runs smoothly and securely, giving you peace of mind letting you focus on your business."
-              className="min-w-[300px] flex-shrink-0"
-              link="/services#block-3"
-            />
-          </div>
+      <div className="flex gap-[19px] min-h-[470px]">
+        <div onClick={() => scrollToId('block-1')} className="flex min-w-[250px]">
+          <Do 
+            icon="/home-do-web.gif"
+            title="Website Design & Development"
+            description="Our Web Design & Development package delivers captivating designs tailored to engage and convert your audience. Each website is crafted with strategic attention to detail, bringing your digital vision to life with a high-performing, user-friendly website on all devices."
+            className="min-w-[300px] flex-shrink-0"
+            link="/services#block-1"
+          />
+        </div>
+        <div onClick={() => scrollToId('block-2')} className="flex min-w-[250px]">
+          <Do 
+            icon="/home-do-brand.gif"
+            title="Branding & Visual Identity Systems"
+            description="Our Branding & Visual Identity Systems package creates a cohesive identity that resonates with your audience. From custom logos to color palettes and iconography, we build a distinctive visual story to elevate your brand and leave a lasting impression across all platforms."
+            className="min-w-[300px] flex-shrink-0"
+            link="/services#block-2"
+          />
+        </div>
+        <div onClick={() => scrollToId('block-3')} className="flex min-w-[250px]">
+          <Do 
+            icon="/home-do-host.gif"
+            title="Hosting, Maintenance & Security"
+            description="Simplify website management with our Hosting, Maintenance, and Security package, which covers updates, security, and protection. Our package ensures that your site runs smoothly and securely, giving you peace of mind letting you focus on your business."
+            className="min-w-[300px] flex-shrink-0"
+            link="/services#block-3"
+          />
         </div>
       </div>
     </div>
