ccc0e4b1f165dc11198778a878c68d38b80d5b59
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-06 10:24:32 -0600

    What we do scroll adjustment v8

--- app/Components/DoContainer/index.js
+++ app/Components/DoContainer/index.js
@@ -21,7 +21,7 @@ export default function DoContainer() {
   };
 
   return (
-    <div className="w-full overflow-x-auto overflow-y-hidden">
+    <div className="w-full overflow-x-scroll overflow-y-hidden">
       <div className='flex gap-5 min-w-full'>
         <div onClick={() => scrollToId('block-1')} className="flex min-w-[250px]">
           <Do 
