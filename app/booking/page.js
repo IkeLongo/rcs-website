e469f340a04c5ba7205967acf235d606c701847e
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-19 10:45:15 -0600

    Booking page v7

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from 'react';
+import Navbar from '../Navbar';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -17,24 +18,22 @@ export default function Booking() {
   }, []);
 
   return (
-    <div className="flex justify-center items-center bg-grey-500">
-      <div className="flex flex-col w-full gap-8 justify-between align-center self-center m-4 pt-5">
-        <Link href="/" passHref>
-          <div className='flex flex-row w-full gap-2 justify-center cursor-pointer'>
-            <Image
-              src="/back arrow.svg"
-              alt="Logo"
-              width={10}
-              height={10}
-            />
-            <p className='self-center font-avenir text-[14px]'>Back to River City Design Studios</p>
-          </div>
-        </Link>
-        <div className='flex flex-col w-full bg-white py-4 rounded-lg'>
-          <h1 className='text-center text-black font-maven-pro tracking-tight pt-2 pb-2 text-[22px] font-bold'>What to discuss our service options?<br />Book a meeting with our team!</h1>
-          <div className="calendly-inline-widget w-full h-screen" data-url="https://calendly.com/isaac-longoria9136/30min?primary_color=46C1E3" style={{ width: '100%', height: '60vh', minWidth: '320px'}}></div>
-        </div>
+    <div className="relative justify-center items-center bg-grey-500">
+    {/* Navbar */}
+    <Navbar />
+
+    <div className="flex flex-col w-full gap-8 justify-between align-center self-center px-4 pt-5 mt-20">
+      <div className="flex flex-col w-full bg-white py-4 rounded-lg max-w-screen mx-auto">
+        <h1 className="text-center text-black font-maven-pro tracking-tight pt-2 pb-2 text-[22px] font-bold mx-8">
+          Want to discuss our service options? Book a meeting with our team!
+        </h1>
+        <div
+          className="calendly-inline-widget w-full h-screen"
+          data-url="https://calendly.com/isaac-longoria9136/30min?primary_color=46C1E3"
+          style={{ width: "100%", height: "60vh", minWidth: "320px" }}
+        ></div>
       </div>
     </div>
+  </div>
   );
 }
