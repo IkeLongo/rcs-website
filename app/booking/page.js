d827158406fc4be89b2bc302fa623623b5ab2bad
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-19 08:58:38 -0600

    Booking page v2

--- app/booking/page.js
+++ app/booking/page.js
@@ -16,8 +16,8 @@ export default function Booking() {
 
   return (
     <div className="flex justify-center items-center bg-grey-500">
-      <div className="flex flex-col w-full h-[800px] m-4">
-        <h1>Book a meeting</h1>
+      <div className="flex flex-col w-full h-screen justify-center align-center m-4">
+        <h1 className='self-center'>Book a meeting</h1>
         <div className="calendly-inline-widget w-full" data-url="https://calendly.com/isaac-longoria9136/30min" style={{ width: '100%', height: '100vh', minWidth: '320px' }}></div>
       </div>
     </div>
