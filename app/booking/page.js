8fa4823ad9532537ada8e4d310495716092fcae2
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-19 11:06:23 -0600

    Booking page v11

--- app/booking/page.js
+++ app/booking/page.js
@@ -23,7 +23,7 @@ export default function Booking() {
       <Navbar />
 
       <div className="w-full h-full gap-8 justify-between align-center self-center px-4 pt-24">
-        <div className="flex flex-col w-full bg-white py-4 rounded-lg max-w-screen mx-auto">
+        <div className="flex flex-col w-full bg-white py-4 rounded-lg max-w-screen md:max-w-[700px] lg:max-w-[1000px] mx-auto">
           <h1 className="text-center text-black font-maven-pro tracking-tight pt-2 pb-2 text-[22px] font-bold mx-8">
             Want to discuss our service options? Book a meeting with our team!
           </h1>
