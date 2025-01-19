5a8ae8718834c9c310f2c73cf29c09ab1c3fa84d
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-19 09:47:12 -0600

    Booking page v3

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Booking() {
   useEffect(() => {
@@ -16,9 +18,22 @@ export default function Booking() {
 
   return (
     <div className="flex justify-center items-center bg-grey-500">
-      <div className="flex flex-col w-full h-screen justify-center align-center m-4">
-        <h1 className='self-center'>Book a meeting</h1>
-        <div className="calendly-inline-widget w-full" data-url="https://calendly.com/isaac-longoria9136/30min" style={{ width: '100%', height: '100vh', minWidth: '320px' }}></div>
+      <div className="flex flex-col w-full justify-between align-center self-center m-4">
+        <Link href="/" passHref>
+          <div className='flex flex-row w-full gap-2 justify-center cursor-pointer'>
+            <Image
+              src="/back arrow.svg"
+              alt="Logo"
+              width={10}
+              height={10}
+            />
+            <p className='self-center'>Back to River City Design Studios</p>
+          </div>
+        </Link>
+        <div className='flex flex-col w-full'>
+          <h1 className='self-center'>Book a Meeting</h1>
+          <div className="calendly-inline-widget w-full h-screen" data-url="https://calendly.com/isaac-longoria9136/30min" style={{ width: '100%', height: '80vh', minWidth: '320px'}}></div>
+        </div>
       </div>
     </div>
   );
