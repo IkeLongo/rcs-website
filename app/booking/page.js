edfe3273a5db38224a943d9b2a7dc58b2da48c5e
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-01-19 10:20:28 -0600

    Booking page v5

--- app/booking/page.js
+++ app/booking/page.js
@@ -18,7 +18,7 @@ export default function Booking() {
 
   return (
     <div className="flex justify-center items-center bg-grey-500">
-      <div className="flex flex-col w-full gap-4 justify-between align-center self-center m-4">
+      <div className="flex flex-col w-full gap-8 justify-between align-center self-center m-4 pt-5">
         <Link href="/" passHref>
           <div className='flex flex-row w-full gap-2 justify-center cursor-pointer'>
             <Image
@@ -27,12 +27,12 @@ export default function Booking() {
               width={10}
               height={10}
             />
-            <p className='self-center'>Back to River City Design Studios</p>
+            <p className='self-center font-avenir text-[14px]'>Back to River City Design Studios</p>
           </div>
         </Link>
         <div className='flex flex-col w-full bg-white py-4 rounded-lg'>
-          <h1 className='text-center text-black font-maven-pro tracking-tighter text-[22px] font-bold'>What to discuss our service options?<br />Book a meeting with our team!</h1>
-          <div className="calendly-inline-widget w-full h-screen" data-url="https://calendly.com/isaac-longoria9136/30min?primary_color=46C1E3" style={{ width: '100%', height: '80vh', minWidth: '320px'}}></div>
+          <h1 className='text-center text-black font-maven-pro tracking-tighter pt-2 pb-2 text-[22px] font-bold'>What to discuss our service options?<br />Book a meeting with our team!</h1>
+          <div className="calendly-inline-widget w-full h-screen" data-url="https://calendly.com/isaac-longoria9136/30min?primary_color=46C1E3" style={{ width: '100%', height: '70vh', minWidth: '320px'}}></div>
         </div>
       </div>
     </div>
