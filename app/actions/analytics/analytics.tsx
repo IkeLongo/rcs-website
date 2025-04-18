e6a81c4dd6c30f181638cc6c39b251a586fb89de
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-04-18 16:46:16 -0500

    Opengraph image adjustments.

--- app/actions/analytics/analytics.tsx
+++ app/actions/analytics/analytics.tsx
@@ -27,7 +27,6 @@ export default function Analytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          console.log("GA initialized: ${GA_ID}");
           gtag('config', '${GA_ID}', {
             page_path: window.location.pathname,
           });
