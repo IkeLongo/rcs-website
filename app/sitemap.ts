d31305d49f220da46f4d3e231752d7b6e3ca613f
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-05-06 13:23:17 -0500

    Updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,37 @@
 import { MetadataRoute } from "next";
 const { SITE_NAME_VAR } = process.env;
 
-export default async function sitempa(): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${SITE_NAME_VAR}`,
-      lastModified: new Date(),
+      lastModified: "2025-05-06",
+      changeFrequency: "monthly", // Indicates the homepage is updated frequently
+      priority: 1.0, // Highest priority for the homepage
     },
     {
       url: `${SITE_NAME_VAR}/services`,
-      lastModified: new Date(),
+      lastModified: "2025-05-06",
+      changeFrequency: "monthly", // Services page might be updated less frequently
+      priority: 0.8,
     },
     {
       url: `${SITE_NAME_VAR}/team`,
-      lastModified: new Date(),
+      lastModified: "2025-05-06",
+      changeFrequency: "monthly", // Team page might change less often
+      priority: 0.6,
     },
     {
       url: `${SITE_NAME_VAR}/terms`,
-      lastModified: new Date(),
+      lastModified: "2025-05-06",
+      changeFrequency: "yearly", // Terms and conditions rarely change
+      priority: 0.3,
     },
     {
       url: `${SITE_NAME_VAR}/privacy`,
-      lastModified: new Date(),
+      lastModified: "2025-05-06",
+      changeFrequency: "yearly", // Privacy policy rarely changes
+      priority: 0.3,
     },
   ];
 }
