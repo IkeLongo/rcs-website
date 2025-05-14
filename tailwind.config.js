d1207b83f6ed4cfee42ed0b0e3543da533d25314
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-05-13 22:05:04 -0500

    Added ui folder path to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./ui/**/*.{js,ts,jsx,tsx}", // ✅ include your custom folder
   ],
   theme: {
     extend: {
