5ceb3871f70e33da62c9b52217e948ec31d1c43d
Isaac Longoria <isaac.longoria9136@gmail.com>
2025-05-13 21:55:21 -0500

    Removed purge to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 const {heroui} = require('@heroui/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
