8f263bc8a56aa4ae75836c992b30f200c861a4ff
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-01-06 16:46:46 -0600

    Removed navbar and toaster

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,14 +1,14 @@
 // app/(site)/layout.tsx
 
-import Navbar from "@/app/ui/layout/navbar/navbar";
-import Toaster from "@/app/ui/components/toaster";
+// import Navbar from "@/app/ui/layout/navbar/navbar";
+// import Toaster from "@/app/ui/components/toaster";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="fixed top-0 z-50 w-full">
-        <Navbar />
-        <Toaster />
+        {/* <Navbar />
+        <Toaster /> */}
       </main>
       <div>
         {children}
