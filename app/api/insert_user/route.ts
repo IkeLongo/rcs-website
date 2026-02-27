999b420b5e91bc090436b0b3b900c92c222e4de3
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-02-27 17:02:05 -0600

    structural changes to website.

--- app/api/insert_user/route.ts
+++ app/api/insert_user/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import pool from '@/app/lib/mysql';
+import pool from '@/lib/mysql';
 import { ResultSetHeader } from 'mysql2';
 
 export async function POST(request: Request) {
